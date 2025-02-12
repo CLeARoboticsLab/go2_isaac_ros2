69506bcb84d21e218629f3a2739ebe8e38a01ae4
jake-levy <jake.j.levy@gmail.com>
2025-02-12 17:19:55 -0600

    launch as process

--- launch/launch_sim.py
+++ launch/launch_sim.py
@@ -1,18 +1,18 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_prefix
+import os
 
 
 def generate_launch_description():
+    package_name = "go2_isaac_ros2"
+    script_path = os.path.join(
+        get_package_prefix(package_name), "lib", package_name, "main.py"
+    )
 
-    sim = Node(
-        package="go2_isaac_ros2",
-        executable="main.py",
-        name="isaac_sim",
+    sim = ExecuteProcess(
+        cmd=[script_path],
         output="screen",
     )
 
-    return LaunchDescription(
-        [
-            sim,
-        ]
-    )
+    return LaunchDescription([sim])
